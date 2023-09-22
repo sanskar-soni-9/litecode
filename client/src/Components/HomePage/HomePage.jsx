c264bb435dc267709dbd5aefe71c8b32bd6a87b2
Immortal <sanskarsoni300@gmail.com>
2023-09-22 22:18:14 +0530

    client - Added litecode demo video and styles HomePage

--- client/src/Components/HomePage/HomePage.jsx
+++ client/src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,29 @@
-import { useEffect } from "react";
+import litecodeGif from "../../assets/litecode.gif";
+import litecodeVid from "../../assets/litecode.mp4";
+
+import "./HomePage.scss";
 
 const HomePage = () => {
-  useEffect(() => {
-    window.location.href = "/problemset/all";
-  });
-
-  return <main></main>;
+  return (
+    <main>
+      <div className="about-container">
+        <div className="about">
+          <h1>Welcome to LiteCode</h1>
+          <p>
+            LiteCode is a working demo of LeetCode, an online platform for
+            coding challenges and interview preparations. LiteCode provides user
+            authentication, code submission and execution features, with a
+            unique architecture involving Kubernetes and RabbitMQ.
+          </p>
+        </div>
+        <img src={litecodeGif} alt="litecode working architecture gif" />
+      </div>
+      <div className="demo-video-container">
+        <h1>Demo Video</h1>
+        <video src={litecodeVid} muted autoPlay loop />
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
